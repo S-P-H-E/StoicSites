2b01643a2d7154d42c579bb68b0f4542cd7bdee9
S-P-H-E <sphe.g.personal@gmail.com>
2023-07-01 12:45:26 +0200

    tech fix

--- components/Tech.js
+++ components/Tech.js
@@ -11,7 +11,7 @@ export default function Tech() {
   const badgeAnimation = useSpring({
     from: { opacity: 0, transform: 'translateY(130px)' },
     to: { opacity: 3, transform: 'translateY(0)' },
-    delay: 3500,
+    delay: 1,
   });
 
   const containerRef = useRef(null);
